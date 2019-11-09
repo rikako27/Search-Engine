8ea3959d77b77d15fe2ed6fdf6d801a56194b12c
Rikako <rikako@vcv073250.vpn.uci.edu>
2019-11-09 12:32:58 -0800

    modify launch.py according to change of Tokenizer class

--- launch.py
+++ launch.py
@@ -1,15 +1,13 @@
 from configparser import ConfigParser
 from argparse import ArgumentParser
-
 from utils.indexer import Indexer
 
 def main(path):
     indexer = Indexer(path)
     indexer.create_indexer()
-    indexer.save_to_file()
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--path", type=str, default="../DEV/aiclub_ics_uci_edu")
+    parser.add_argument("--path", type=str, default="../DEV") # /home/lopes/Datasets/IR/DEV
     args = parser.parse_args()
     main(args.path)
