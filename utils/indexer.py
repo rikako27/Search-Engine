8acfa9229de487e5116e3f2adf319f1f0fefe497
Rikako Yamamoto <rikakoy@circinus-41.ics.uci.edu>
2019-11-10 01:56:30 -0800

    add pickle

--- utils/indexer.py
+++ utils/indexer.py
@@ -25,7 +25,7 @@ class Indexer:
             os.makedirs(self.data_store)
 
         for key in self.data.keys():
-            file = self.data_store + "/" + str(key)
+            file = self.data_store + "/" + str(key)  
             if os.path.exists(file):
                 os.remove(file)
 
@@ -36,8 +36,7 @@ class Indexer:
         for t, tf in token_tf.items():
             self.data[t[0]][t].append((doc_id, tf))
 
-        if self.count_files != 0 and self.count_files % 100 == 0:
-            print("count_files %d\n" % self.count_files)
+        if self.count_files != 0 and self.count_files % 300 == 0:
             self.save_to_file()
 
     def removeFragment(self, url:str):
@@ -49,7 +48,6 @@ class Indexer:
         for dir in self.path_to_db.iterdir():
             if dir.is_dir():
                 for file in dir.iterdir():
-                    gc.collect()
                     if not file.is_file():
                         continue
         #for file in self.path_to_db.iterdir():
@@ -57,11 +55,12 @@ class Indexer:
                         parsed_json = json.load(file)
                         url = self.removeFragment(parsed_json['url'])
                         content = parsed_json['content']
-
+                    
                     hash_doc.write("%d, %s\n" % (self.count_files, url))
                     tokenizer = Tokenizer(content)
                     token_tf = tokenizer.extract_texts()
                     self.add_tokens_to_dictionary(token_tf, self.count_files)
+                    gc.collect() 
                     self.count_files += 1
         hash_doc.close()
         self.save_to_file()
@@ -73,14 +72,14 @@ class Indexer:
             file = self.data_store + "/" + key
 
             if not os.path.exists(file):
-                with open(file, "w+") as write_file:
-                    json.dump(self.data[key], write_file)
+                with open(file, "wb") as write_file:
+                    pickle.dump(self.data[key], write_file,  protocol=pickle.HIGHEST_PROTOCOL)
                 self.data[key].clear()
                 self.data[key] = defaultdict(list)
 
             else: #need merging
-                with open(file, "r") as f: #grap the saved dictionary
-                    orig_dict = json.load(f)
+                with open(file, "rb") as f: #grap the saved dictionary
+                    orig_dict = pickle.load(f)
 
                 #Now appending
                 for token, posting in self.data[key].items():
@@ -90,17 +89,17 @@ class Indexer:
                 self.data[key].clear()
                 self.data[key] = defaultdict(list)
 
-                with open(file, "w+") as write_file:
-                    json.dump(orig_dict, write_file)
-                orig_dict = {}
-
+                with open(file, "wb") as write_file:
+                    pickle.dump(orig_dict, write_file, protocol=pickle.HIGHEST_PROTOCOL)
+                orig_dict = {} 
+            gc.collect()
         print("the number of documents %d\n" % self.count_files)
 
     def recalculate_tf_idf(self):
         for key in self.data.keys():
-            file = self.data_store + "/" + key
-            with open(file, "r") as rf:
-                token_posting = json.load(rf)
+            file = self.data_store + "/" + key 
+            with open(file, "rb") as rf:
+                token_posting = pickle.load(rf)
             os.remove(file)
             for token, posting in token_posting.items():
                 num_doc = len(posting)
@@ -108,6 +107,6 @@ class Indexer:
                     idf = log(1.0 * self.count_files / num_doc)
                     tf_idf = idf * elem[1]
                     token_posting[token][index][1] = round(tf_idf + 0.005, 2)
-            with open(file, "w+") as wf:
-                json.dump(token_posting, wf)
-            token_posting = {}
+            with open(file, "wb") as wf:
+                pickle.dump(token_posting, wf, protocol=pickle.HIGHEST_PROTOCOL)
+            gc.collect()
